dd76ef02a37244d51e785aae2af971548f235377
Flofrost <ballagerflorian@pm.me>
2025-07-12 18:22:31 +0200

    finished css for bingo page?

--- static/script/bingo.js
+++ static/script/bingo.js
@@ -1,42 +1,59 @@
 const socket = io()
 
-const name = localStorage.getItem('name');
+const currentPlayerName = localStorage.getItem('name');
 
 function connect(name){
     console.log("yeah");
     socket.emit('join', { name: name });
 }
 
+/** @param {HTMLElement} element */
+function findBingoIndex(element){
+    for(let i = 0; i < bingoGrid.children.length; i++)
+        if(bingoGrid.children[i] == element)
+            return i;
+    return undefined
+}
+
 window.onload = () => {
-    const view = {
-        redTeam : teamRed,
-        blueTeam : teamBlue,
-        greyTeam : teamNeutral,
+    const teamsDisplay = {
+        red : teamRed,
+        blu : teamBlue,
+        neutral : teamNeutral,
     };
 
     socket.on('update_connected_users', data => {
-        view.greyTeam.classList.add("hidden");
-        view.redTeam.innerHTML = "";
-        view.blueTeam.innerHTML = "";
-        view.greyTeam.innerHTML = "";
+        teamsDisplay.red.innerHTML = '<div style="flex-grow: 1;"></div>';
+        teamsDisplay.blu.innerHTML = '<div style="flex-grow: 1;"></div>';
+        teamsDisplay.neutral.innerHTML = '<div style="flex-grow: 1;"></div>';
 
         data.forEach(user => {
-            if(user["team"] === "red"){
-                view.redTeam.appendChild(document.createElement("h2").appendChild(document.createTextNode(user["name"])));
-            }else if(user["team"] === "blu"){
-                view.blueTeam.appendChild(document.createElement("h2").appendChild(document.createTextNode(user["name"])));
-            }else{
-                if(view.greyTeam.classList.contains("hidden")){
-                    view.greyTeam.classList.remove("hidden");
-                }
-                view.greyTeam.appendChild(document.createElement("h2").appendChild(document.createTextNode(user["name"])));
-            }
+            const nameDisplay = document.createElement("p");
+            nameDisplay.textContent = user.name;
+
+            if(!Object.keys(teamsDisplay).includes(user.team)) user.team = "neutral";
+
+            teamsDisplay[user.team].appendChild(nameDisplay);
+       
         })
+
+        teamsDisplay.red.innerHTML += '<div style="flex-grow: 1;"></div>';
+        teamsDisplay.blu.innerHTML += '<div style="flex-grow: 1;"></div>';
+        teamsDisplay.neutral.innerHTML += '<div style="flex-grow: 1;"></div>';
     });
 
-    connect(name);
+    for(const child of bingoGrid.children)
+        child.addEventListener("click", e => {
+            const ii = findBingoIndex(e.target);
+            if(ii != undefined){
+                console.log(ii);
+            }
+        });
+    
+
+    connect(currentPlayerName);
 }
 
 window.onbeforeunload = () => {
-    socket.emit('leave', {"name" : name})
+    socket.emit('leave', {"name" : currentPlayerName});
 }
